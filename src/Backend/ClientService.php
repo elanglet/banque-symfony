96265c333bee3c429625bef617486ad0adccca76
Etienne LANGLET <elanglet@eni.fr>
2021-09-01 15:55:58 +0200

    Ajout des services backend (Doctrine)

---
+++ src/Backend/ClientService.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Backend;
+
+use App\Entity\Client;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ClientService
+{
+
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    
+    public function ajouterClient(Client $client) : void 
+    {
+        $this->em->persist($client);
+        $this->em->flush();
+    }
+    
+    public function modifierClient(Client $client) : void
+    {
+        $this->em->merge($client);
+        $this->em->flush();
+    }
+    
+    public function supprimerClient(int $id) : void
+    {
+        $client = $this->em->getRepository('App:Client')->find($id);
+        $this->em->remove($client);
+        $this->em->flush();
+    }
+    
+    public function rechercherTousLesClients() : array 
+    {
+        return $this->em->getRepository('App:Client')->findAll();
+    }
+    
+    public function rechercherClientParId(int $id) : Client
+    {
+        return $this->em->getRepository('App:Client')->find($id);
+    }
+    
+    public function rechercherClientsParNomEtPrenom(string $nom, string $prenom) : array 
+    {
+        $query = $this->em->getRepository('App:Client')->createQuery('SELECT a FROM App:Client a WHERE a.nom=:nom AND a.prenom=:prenom');
+        $query->setParameter(':nom', $nom);
+        $query->setParameter(':prenom', $prenom);
+        
+        return $query->getResult();
+    }
+}
+
---
+++ src/Backend/CompteService.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Backend;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Compte;
+use App\Entity\Client;
+
+class ClientService
+{
+
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    
+    public function ajouterCompte(Compte $compte) : void
+    {
+        $this->em->persist($compte);
+        $this->em->flush();
+    }
+    
+    public function modifierCompte(Compte $compte) : void
+    {
+        $this->em->merge($compte);
+        $this->em->flush();
+    }
+    
+    
+    public function supprimerCompte(int $numero) : void 
+    {
+        $compte = $this->em->getRepository('App:Compte')->find($numero);
+        $this->em->remove($compte);
+        $this->em->flush();
+    }
+    
+    public function rechercherCompteParNumero(int $numero) : Compte
+    {
+        return $this->em->getRepository('App:Compte')->find($numero);
+    }
+    
+    public function rechercherComptesClient(Client $client) : array 
+    {
+        $query = $this->em->getRepository('App:Compte')->createQuery-('SELECT c FROM App:Compte c WHERE c.client=:client');
+        $query->setParameter(':client', $client);
+        return $query->getResult();
+    }
+    
+    public function rechercherComptesDebiteurs() : array 
+    {
+        $query = $this->em->getRepository('App:Compte')->createQuery-('SELECT c FROM App:Compte c WHERE c.solde < 0');
+        return $query->getResult();
+    }
+
+}
+
