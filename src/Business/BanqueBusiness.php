fd5f079ae54783ae687b2f1e03a222142be95296
Etienne LANGLET <elanglet@eni.fr>
2021-09-05 19:03:26 +0200

    Ajout du composant BanqueBusiness

--- src/Business/BanqueBusiness.php
+++ src/Business/BanqueBusiness.php
@@ -19,30 +19,44 @@ class BanqueBusiness
     public function authentifier(int $idClient, string $motDePasse) : Client
     {
         try {
-            
+            $client = $this->clientService->rechercherClientParId($idClient);
+            if($client != null && $motDePasse != null && $motDePasse === $client->getCodepostal()) {
+                return $client;
+            }
+            else {
+                throw new \Exception();
+            }
         } 
         catch (\Exception $e) {
-        
+            throw new \Exception("Erreur d'authentification.");
         }
     }
     
     public function mesComptes(int $idClient) : array
     {
         try {
-            
+            $client = $this->clientService->rechercherClientParId($idClient);
+            return $this->compteService->rechercherComptesClient($client);
         }
         catch (\Exception $e) {
-            
+            throw new \Exception("Erreur de récupération des comptes.");
         }
     }
     
     public function virement(int $numeroDebit, int $numeroCredit, float $montant) : void
     {
         try {
+            $compteADebiter = $this->compteService->rechercherCompteParNumero($numeroDebit);
+            $compteACrediter = $this->compteService->rechercherCompteParNumero($numeroCredit);
             
+            $compteADebiter->setSolde($compteADebiter->getSolde() - $montant);
+            $compteACrediter->setSolde($compteACrediter->getSolde() + $montant);
+            
+            $this->compteService->modifierCompte($compteADebiter);
+            $this->compteService->modifierCompte($compteACrediter);
         }
         catch (\Exception $e) {
-            
+            throw new \Exception("Erreur d'enregistrement du virement.");
         }
     }
 }
